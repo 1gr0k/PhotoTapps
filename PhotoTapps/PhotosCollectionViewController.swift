5e3b7016c9173f2e94be5ab117b7f2b8be64f200
Andrey Kalyamin <kalyamin.andrey@gmail.com>
2021-02-28 23:23:08 +0400

    flow layout

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -41,5 +41,24 @@ class PhotosCollectionViewController: UICollectionViewController {
 }
 
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 3
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        let avaliableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = avaliableWidth / itemsPerRow
+        print ("\(itemsPerRow), \(paddingWidth), \(avaliableWidth), \(widthPerItem), \(collectionView.frame.width)")
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
 }
